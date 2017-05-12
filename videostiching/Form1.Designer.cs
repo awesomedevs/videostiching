08abe4710a9c06213a43d7440bf48937006d5560
Filipe Fernandes <sirfilipef@gmail.com>
2017-05-12 15:09:14 +0100

    Added Sticher

--- videostiching/Form1.Designer.cs
+++ videostiching/Form1.Designer.cs
@@ -28,20 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.stichFirstFrameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1906, 1121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1906, 38);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFilesToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 34);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stichFirstFrameToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(72, 34);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(82, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openFilesToolStripMenuItem
+            // 
+            this.openFilesToolStripMenuItem.Name = "openFilesToolStripMenuItem";
+            this.openFilesToolStripMenuItem.Size = new System.Drawing.Size(239, 34);
+            this.openFilesToolStripMenuItem.Text = "Open Files";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // stichFirstFrameToolStripMenuItem
+            // 
+            this.stichFirstFrameToolStripMenuItem.Name = "stichFirstFrameToolStripMenuItem";
+            this.stichFirstFrameToolStripMenuItem.Size = new System.Drawing.Size(255, 34);
+            this.stichFirstFrameToolStripMenuItem.Text = "Stich First Frame";
+            this.stichFirstFrameToolStripMenuItem.Click += new System.EventHandler(this.stichFirstFrameToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1350, 802);
+            this.ClientSize = new System.Drawing.Size(1906, 1159);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Video Stiching EmguCV";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFilesToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem stichFirstFrameToolStripMenuItem;
     }
 }
 
--- videostiching/Form1.cs
+++ videostiching/Form1.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Emgu.CV;
+using Emgu.CV.Stitching;
+using Emgu.CV.Util;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace videostiching
@@ -15,6 +13,52 @@ namespace videostiching
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void stichFirstFrameToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.Multiselect = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                Mat[] sourceImages = new Mat[2];
+                Mat result = new Mat();
+
+                Capture capt1 = new Capture(dlg.FileNames[0].ToString());
+                Capture capt2 = new Capture(dlg.FileNames[1].ToString());
+                sourceImages[0] = capt1.QueryFrame();
+                sourceImages[1] = capt2.QueryFrame();
+                try
+                {
+                    using (Stitcher stitcher = new Stitcher(true))
+                    {
+                        using (VectorOfMat vm = new VectorOfMat())
+                        {
+                            vm.Push(sourceImages);
+
+                            var stitchStatus = stitcher.Stitch(vm, result);
+
+                            if (stitchStatus)
+                            {
+                                Bitmap bt = new Bitmap(result.Bitmap);
+                                bt.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+                                pictureBox1.Image = bt;
+                                pictureBox1.Image.Save(@"C:\Users\lanun-surf\Desktop\stich.jpeg", ImageFormat.Jpeg);
+                            }
+                            else
+                            {
+                                MessageBox.Show(this, String.Format("Stiching Error: {0}", stitchStatus));
+                                pictureBox1.Image = null;
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                }
+            }
         }
     }
 }
